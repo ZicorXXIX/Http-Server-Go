8538195bfd3b3a1f8e570dcefe2aa0d3c7ab3b58
Manoj SInnur <manoj19.sinnur@gmail.com>
2024-04-14 23:22:04 +0530

    pass stage

--- app/server.go
+++ app/server.go
@@ -47,8 +47,8 @@ func handleConnection(conn net.Conn) {
 		} else if strings.Contains(route, "/echo"){
 			responseBody := strings.Split(route, "echo/")[1]
 			fmt.Println(responseBody)
-			// response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%d\r\n\r\n%s", len(responseBody), responseBody)
-			sendResponse(conn, "HTTP/1.1 404 Not Found\r\n\r\n404 Not Found")
+			response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%d\r\n\r\n%s", len(responseBody), responseBody)
+			sendResponse(conn, response)
 		} else if route == "/user-agent" {
 			// parseUserAgent(conn)
 			// fmt.Println(userAgent)
